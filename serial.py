712067748a8db4249c03d6904ac23da928e05659
wrserne <110362301+wrserne@users.noreply.github.com>
2022-10-10 21:37:51 -0400

    Add files via upload

---
+++ serial.py
@@ -0,0 +1,17 @@
+"""Python serial number generator."""
+
+class SerialGenerator:
+
+    def __init__(self, start=0):
+        self.start = self.next = start
+    def __repr__(self):
+        return f"<SerialGenerator start= {self.start} next={self.next}>"
+    
+    def generate(self):
+
+        self.next +=1
+        return self.next - 1
+
+    def reset(self):
+
+        self.next = self.start
