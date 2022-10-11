f2a3d576fecfedf81a667762a207ea1697e3430e
wrserne <110362301+wrserne@users.noreply.github.com>
2022-10-10 21:40:00 -0400

    Add files via upload

--- wordfinder.py
+++ wordfinder.py
@@ -1,5 +1,23 @@
 """Word Finder: finds random words from a dictionary."""
 
+import random
 
 class WordFinder:
-    ...
+
+    def __init__(self,path):
+    
+        dict_file = open(path)
+
+        self.words= self.parse(dict_file)
+
+        print(f"{len(self.words)} words read")
+
+    def parse(self, dict_file):
+        """Parse dict_file -> list of words."""
+
+        return [w.strip() for w in dict_file]
+
+    def random(self):
+        """Return random word."""
+
+        return random.choice(self.words)
